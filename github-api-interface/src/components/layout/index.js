4da6d661cfc64ed5e6d5a4661ebdcddc21442c38
Walessonrdreis <wallllase@hotmail.com>
2022-07-24 16:04:39 -0300

    Componente Layout, e pequenas correcoes de erros corrigidas

--- github-api-interface/src/components/layout/index.js
+++ github-api-interface/src/components/layout/index.js
@@ -4,6 +4,11 @@ import * as S from "./styled";
 
 const Layout = ({children}) => {
   return (
-    <
+    <S.WrapperLayout>
+        <Header />
+        {children}
+    </S.WrapperLayout>
   );
 }
+
+export default Layout;

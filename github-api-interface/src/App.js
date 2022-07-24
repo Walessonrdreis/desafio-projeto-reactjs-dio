b5ed0364249e60c7e521a1ffc79d247b2ff2d836
Walessonrdreis <wallllase@hotmail.com>
2022-07-24 15:53:46 -0300

    Criando componete header completo, e inserindo algoritmo do App

--- github-api-interface/src/App.js
+++ github-api-interface/src/App.js
@@ -6,12 +6,27 @@ import Repositories from "./components/repositories";
 import useGithub from "./hooks/github-hooks";
 
 const App = () => {
+  const {githubState} = useGithub();
  
   return(
-
-
+    <Layout>
+    {githubState.hasUser ? (
+      <>
+        {githubState.loading ? (
+          <p>Loading</p>
+        ): (
+          <>
+            <Profile />
+            <Repositories />
+          </>
+        )}
+      </>
+    ):(
+      <NoSearch />
+    )}
+    </Layout>
   );
 }
 
-
+export default App;
 
